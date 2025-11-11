21d6969b67b2db9ec5e91a49cfb5acb0930f5489
Chris Salthouse <salthous@gmail.com>
2025-11-11 10:37:13 -0600

    Removed version numbers of src components

--- src/components/StepsPane.tsx
+++ src/components/StepsPane.tsx
@@ -14,6 +14,7 @@ interface StepsPaneProps {
 }
 
 export function StepsPane({ steps, onToggleCompleted }: StepsPaneProps) {
+  console.log(onToggleCompleted);
   return (
     <div className="h-full bg-gray-50 border-l border-gray-200 flex flex-col">
       <div className="px-6 py-4 border-b border-gray-200 bg-white">
